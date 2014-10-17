d93c67a35d0b6c715d1e2f2954a13b99b939da1b
ikoddn <ikonage@gmail.com>
2014-10-17 16:29:56 +0200

    AccountReplicaDAO interface

--- INF5040-Obl2/src/no/uio/inf5040/obl2/client/dao/AccountReplicaDAO.java
+++ INF5040-Obl2/src/no/uio/inf5040/obl2/client/dao/AccountReplicaDAO.java
@@ -2,4 +2,11 @@ package no.uio.inf5040.obl2.client.dao;
 
 public interface AccountReplicaDAO {
 
+	void addInterest(double percent) throws DAOException;
+	
+	double getBalance() throws DAOException;
+	
+	void deposit(double amount) throws DAOException;
+	
+	void withdraw(double amount) throws DAOException;
 }
