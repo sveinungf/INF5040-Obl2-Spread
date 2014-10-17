c6d308f99f65e35bff7a89d736a421f03861f4d8
ikoddn <ikonage@gmail.com>
2014-10-17 12:47:33 +0200

    Account bean and JUnit test

---
+++ INF5040-Obl2/src/no/uio/inf5040/obl2/client/Account.java
@@ -0,0 +1,30 @@
+package no.uio.inf5040.obl2.client;
+
+public class Account {
+
+	private double balance;
+
+	public Account() {
+		balance = 0.0;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+	
+	public void addInterest(double percent) {
+		balance *= 1 + percent / 100;
+	}
+}
---
+++ INF5040-Obl2/test/no/uio/inf5040/obl2/client/AccountTest.java
@@ -0,0 +1,17 @@
+package no.uio.inf5040.obl2.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccountTest {
+
+	@Test
+	public void addInterest() {
+		Account account = new Account();
+		account.setBalance(100.0);
+		account.addInterest(7);
+		
+		assertEquals(107.0, account.getBalance(), 0.001);
+	}
+}
