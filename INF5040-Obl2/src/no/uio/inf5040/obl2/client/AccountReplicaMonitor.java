667cd1e72cc10026eaf9cfbc2c52c10ce4f29bdc
Halvor Kielland <hkiella@gmail.com>
2014-11-05 11:54:51 +0100

    Javadoc written

--- INF5040-Obl2/src/no/uio/inf5040/obl2/client/AccountReplicaMonitor.java
+++ INF5040-Obl2/src/no/uio/inf5040/obl2/client/AccountReplicaMonitor.java
@@ -1,11 +1,22 @@
 package no.uio.inf5040.obl2.client;
 
+/**
+ * Simple class for making the main thread wait until
+ * the required amount of replicas are present
+ *
+ */
 public class AccountReplicaMonitor {
 
+	/**
+	 * Waits on this object
+	 */
 	public synchronized void doWait() throws InterruptedException {
 		wait();
 	}
 
+	/**
+	 * Notifies thread waiting on this object
+	 */
 	public synchronized void doNotify() {
 		notify();
 	}
